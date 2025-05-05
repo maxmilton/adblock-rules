c2d43a4c36ce4621faae14710c55d2a2b58c799e
Max Milton <max@happyhackers.xyz>
2025-05-05 18:23:34 +0900

    batch

--- launch-adblock-test.sh
+++ launch-adblock-test.sh
@@ -5,8 +5,9 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
 if ! test -f "${script_dir}/favicon.ico"; then
   # Create minimal favicon.ico file
-  magick -size 1x1 xc:none "${script_dir}/favicon.ico"
+  magick -size 16x16 xc:red -strip -colors 1 -depth 2 "${script_dir}/favicon.ico"
 fi
 
-xdg-open 'http://localhost:5000/adblock-test.html'
+url='http://localhost:5000/adblock-test.html'
+xdg-open "$url" 2>/dev/null || echo "Open URL: $url"
 exec busybox httpd -fvv -p '127.0.0.1:5000' -h "$script_dir"
